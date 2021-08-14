9a587f954d5aba3c792f748e292abc31e265e273
Sam Hepburn <samhep@me.com>
2021-08-14 17:14:20 +0100

    added Get-IntuneWin32Tool

--- PackageHelper.psm1
+++ PackageHelper.psm1
@@ -1,6 +1,19 @@
 ﻿
 function Get-RegInformation {
-    Try {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+    try {
             $regApps = New-Object System.Collections.ArrayList
             $regKeys = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall", "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall"
             Get-ChildItem $regKeys -rec -ea SilentlyContinue | ForEach-Object { 
@@ -31,7 +44,7 @@ function Get-RegInformation {
             Return $regApps
 
     }
-    Catch {
+    catch {
 
             $errorMessage = $_.Exception.Message  
             $errorValue = "Error"
@@ -62,6 +75,22 @@ function Get-RegInformation {
 }
 
 Function Get-ApplicationInfo {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .PARAMETER SearchTerm
+    Parameter description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
@@ -102,6 +131,25 @@ Function Get-ApplicationInfo {
 }
 
 function Export-ApplicationConfig {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .PARAMETER obj
+    Parameter description
+    
+    .PARAMETER FileType
+    Parameter description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
 
     [CmdletBinding()] 
     Param(
@@ -110,7 +158,7 @@ function Export-ApplicationConfig {
     )
     Process{
         $obj | ForEach-Object {
-            Try {       
+            try {       
 
                 switch ($FileType) {
                     json { 
@@ -119,10 +167,64 @@ function Export-ApplicationConfig {
                     Default { $obj | ConvertTo-Json | Out-File -FilePath "C:\PackageHelper\Export\$($obj.DisplayName).json" -Verbose }
                 }
             }
-            Catch{}
+            catch{}
         }
     }
  
+}
+
+function Get-IntuneWin32Tool {
+    <#
+    .SYNOPSIS
+    Short description
+    
+    .DESCRIPTION
+    Long description
+    
+    .EXAMPLE
+    An example
+    
+    .NOTES
+    General notes
+    #>
+
+    try {
+
+        # See if tool is already available
+
+        if (Test-Path -Path "C:\PackageHelper\Tools\IntuneWinAppUtil\IntuneWinAppUtil.exe") {
+            Write-Host "Intune Win32 Util is already available."
+        } else {
+            # Check for paths
+
+            if (!(Test-Path -Path "C:\PackageHelper\Tools")) {
+                New-Item -Path "C:\PackageHelper\" -ItemType Directory -Name "Tools"
+            }
+            if (!(Test-Path -Path "C:\PackageHelper\Temp")) {
+                New-Item -Path "C:\PackageHelper\" -ItemType Directory -Name "Temp"
+            }
+            if (!(Test-Path -Path "C:\PackageHelper\Temp\IntuneWinAppUtil")) {
+                New-Item -Path "C:\PackageHelper\Temp" -ItemType Directory -Name "IntuneWinAppUtil"
+            }
+
+            Invoke-WebRequest -Uri "https://github.com/microsoft/Microsoft-Win32-Content-Prep-Tool/archive/refs/tags/1.8.3.zip" -OutFile "C:\PackageHelper\Temp\IntuneWinAppUtil\IntuneWin32Tool.zip" 
+
+            Expand-Archive -Path "C:\PackageHelper\Temp\IntuneWinAppUtil\IntuneWin32Tool.zip" -DestinationPath "C:\PackageHelper\Temp\IntuneWinAppUtil\IntuneWin32Tool"
+
+            if (!(Test-Path -Path "C:\PackageHelper\Tools\IntuneWinAppUtil")) {
+                New-Item -Path "C:\PackageHelper\Tools\" -ItemType Directory -Name "IntuneWinAppUtil"
+            }
+
+            Copy-Item -Path "C:\PackageHelper\Temp\IntuneWinAppUtil\IntuneWin32Tool\Microsoft-Win32-Content-Prep-Tool-1.8.3\IntuneWinAppUtil.exe" -Destination "C:\PackageHelper\Tools\IntuneWinAppUtil\IntuneWinAppUtil.exe"
+
+            Remove-Item -Path "C:\PackageHelper\Temp\IntuneWinAppUtil" -Recurse
+        }
+     
+
+    }
+    catch {
+        
+    }
 }
 
 Export-ModuleMember -Function Get-ApplicationInfo, Export-ApplicationConfig
